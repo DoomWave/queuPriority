ef261edc8e4d7e34eb7f859750f5ea95158006d3
Najair Acuna <123853739+DoomWave@users.noreply.github.com>
2026-05-26 18:57:29 -0700

    messy around with the lost light

--- src/main.java
+++ src/main.java
@@ -2,19 +2,29 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
+class LostLight{
+    String name;
+    int rank;
+
+    LostLight(String name,int rank){
+        this.name=name;
+        this.rank=rank;
+    }
+}
 
 public class main {
     public static void main(String[] args) {
-        Queue<String> queue = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<LostLight> queue = new PriorityQueue<>((a,b)-> Integer.compare(b.rank, a.rank));
 
-        queue.offer("Rodimus");
-        queue.offer("Ultra Magnus");
-        queue.offer("Drift");
-        queue.offer("Ratchet");
-        queue.offer("Perceptor");
+        queue.offer(new LostLight("Rodimus", 76));
+        queue.offer(new LostLight("Ultra Magnus", 100));
+        queue.offer(new LostLight("Drift", 45));
+        queue.offer(new LostLight("Ratchet", 89));
+        queue.offer(new LostLight("Perceptor", 30));
 
         while(!queue.isEmpty()){
-            System.out.println(queue.poll());
+            LostLight t = queue.poll();
+            System.out.println(t.name + " : " + t.rank);
         }
     }
 }
