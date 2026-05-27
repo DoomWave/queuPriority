197232d556d064fb4e592d7424a3fef2f7839136
Najair Acuna <123853739+DoomWave@users.noreply.github.com>
2026-05-26 18:45:40 -0700

    Finished of learning about priority queues, time to messy around.

--- src/main.java
+++ src/main.java
@@ -1,5 +1,20 @@
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class main {
     public static void main(String[] args) {
+        Queue<String> queue = new PriorityQueue<>(Collections.reverseOrder());
 
+        queue.offer("Rodimus");
+        queue.offer("Ultra Magnus");
+        queue.offer("Drift");
+        queue.offer("Ratchet");
+        queue.offer("Perceptor");
+
+        while(!queue.isEmpty()){
+            System.out.println(queue.poll());
+        }
     }
 }
